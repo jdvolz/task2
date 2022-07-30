4e650da27ddeb2e8c70950747279ca2391479473
Joshua Volz <jdvolz@gmail.com>
2022-07-30 13:51:59 -0700

    Initial Add test passes

--- task2.go
+++ task2.go
@@ -2,7 +2,33 @@ package main
 
 type DoubleSet map[int]int
 
-// func (ds1 DoubleSet) Add(ds2 DoubleSet) DoubleSet {
+func (ds1 DoubleSet) Add(ds2 DoubleSet) DoubleSet {
+  ds := make(DoubleSet)
+
+  for k, v := range ds1 {
+    v2, ok := ds2[k]
+    var cv int
+    if ok {
+      cv = v + v2
+    } else {
+      cv = v
+    }
+
+    if cv >= 2 {
+      ds[k] = 2
+    } else if cv == 1 {
+      ds[k] = 1
+    }
+  }
+
+  for k2, v2 := range ds2 {
+    if _, ok := ds[k2]; !ok {
+      ds[k2] = v2
+    }
+  }
+
+  return ds
+}
 //   ds := DoubleSet
 //   for k, v := range ds1 {
 //
--- task2_test.go
+++ task2_test.go
@@ -23,3 +23,25 @@ func TestSubtraction(t *testing.T) {
 		t.Fatalf("result has a different length than the target")
 	}
 }
+
+func TestAdd(t *testing.T) {
+	doubleSetOne := DoubleSet{1: 2, 2: 1}
+	doubleSetTwo := DoubleSet{1: 1, 2: 1, -3: 1}
+
+	result := doubleSetOne.Add(doubleSetTwo)
+
+	target := DoubleSet{1: 2, 2: 2, -3: 1}
+
+	for tk, tv := range target {
+		if v, ok := result[tk]; !ok {
+			t.Fatalf("%v is a missing key", tk)
+		} else {
+			if v != tv {
+				t.Fatalf("1 has value %v when it should be %v", v, tv)
+			}
+		}
+	}
+	if len(result) != len(target) {
+		t.Fatalf("result has a different length than the target")
+	}
+}
